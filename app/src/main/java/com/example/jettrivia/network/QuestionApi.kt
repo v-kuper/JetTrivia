cfc00bd148d8c3adec254b04852b0e942db66e04
v-kuper <vkuper7@gmail.com>
2025-09-14 15:20:48 +0300

    feat: add Retrofit.Builder

---
+++ app/src/main/java/com/example/jettrivia/network/QuestionApi.kt
@@ -0,0 +1,11 @@
+package com.example.jettrivia.network
+
+import com.example.jettrivia.model.Question
+import retrofit2.http.GET
+import javax.inject.Singleton
+
+@Singleton
+interface QuestionApi {
+    @GET("world.json")
+    suspend fun getAllQuestions(): Question
+}
