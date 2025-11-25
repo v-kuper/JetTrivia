b2ff414252112ac2d48f7733312089ae7187190c
v-kuper <vkuper7@gmail.com>
2025-11-25 23:09:14 +0300

    feat: add DrawDottedLine

--- app/src/main/java/com/example/jettrivia/component/Questions.kt
+++ app/src/main/java/com/example/jettrivia/component/Questions.kt
@@ -1,10 +1,13 @@
 package com.example.jettrivia.component
 
 import android.util.Log
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
@@ -13,6 +16,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,6 +46,7 @@ fun Questions(viewModel: QuestionsViewModel) {
 @Preview
 @Composable
 fun QuestionsDisplay() {
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
     Surface(modifier = Modifier
         .fillMaxSize()
         .fillMaxSize()
@@ -50,9 +56,30 @@ fun QuestionsDisplay() {
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start) {
             QuestionTracker()
+            DrawDottedLine(pathEffect = pathEffect)
         }
     }
 }
+
+@Composable
+fun DrawDottedLine(pathEffect: PathEffect) {
+    Canvas(modifier = Modifier.fillMaxWidth().height(1.dp)
+        ) {
+        drawLine(
+            color = AppColors.mLightGray,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, 0f),
+            pathEffect = pathEffect
+        )
+    }
+}
+
+
+
+
+
+
+
 
 @Composable
 fun QuestionTracker(counter: Int = 10, outOf: Int = 100) {
